f2f4fd4f4a8f00914bf68874b532c9e58e327efd
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-20 14:53:31 -0500

    update feature added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
   post 'pictures'=> 'pictures#create'
-    get 'pictures/new' => 'pictures#new'
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
+  get 'pictures/new' => 'pictures#new'
   get 'pictures/:id' => 'pictures#show'
 
 
