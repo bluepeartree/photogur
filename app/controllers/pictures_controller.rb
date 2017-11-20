b6e7a27052aa52281611c1423f9354345d38b36d
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-20 14:30:07 -0500

    new picture added

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,4 +8,13 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
+  def new
+    #we're making a brand new instance of the Picture model that will help us set up a form in our view.
+    @picture = Picture.new
+  end
+
+  def create
+    render text: "Received POST request to '/pictures' with the data URL: #{params}"
+  end
+
 end
