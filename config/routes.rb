c91f56596018e5df3105e2958f66e20a43f16d74
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-20 14:58:22 -0500

    delete functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
   post 'pictures'=> 'pictures#create'
-  get 'pictures/:id/edit' => "pictures#edit"
-  patch 'pictures/:id' => "pictures#update"
+  get 'pictures/:id/edit' => 'pictures#edit'
+  patch 'pictures/:id' => 'pictures#update'
+  delete 'pictures/:id' => "pictures#destroy"
   get 'pictures/new' => 'pictures#new'
   get 'pictures/:id' => 'pictures#show'
 
