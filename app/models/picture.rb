4ff13e1ec8594d84b494f6c64e623ac461ff6bc5
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-27 17:45:12 -0500

    added validation + error message

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ApplicationRecord
+  validates :artist, :title, :url, presence: true
+  validates :title, length: { in: 3..20 }
+  validates :url, uniqueness: true
 
   def self.newest_first
     Picture.order("created_at DESC")
