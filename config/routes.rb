1390581d70f82cb9bb2df4728c8dd65443fdcd5e
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-29 13:51:37 -0500

    add user registration functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,12 @@ Rails.application.routes.draw do
 
   get 'pictures/:id/edit' => 'pictures#edit'
   patch 'pictures/:id' => 'pictures#update'
-  
+
   delete 'pictures/:id' => "pictures#destroy"
 
   get 'pictures/:id' => 'pictures#show'
+
+  resources :users
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
