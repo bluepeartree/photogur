2ac54e2683cb54793e8bb079994a89bac9a24a9f
bluepeartree <sherrie.sebastian@gmail.com>
2017-12-01 13:04:52 -0500

    added one-to-many relationship between user and pics

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,7 @@ class Picture < ApplicationRecord
   validates :artist, :title, :url, presence: true
   validates :title, length: { in: 3..20 }
   validates :url, uniqueness: true
+  belongs_to :user
 
   def self.newest_first
     Picture.order("created_at DESC")
